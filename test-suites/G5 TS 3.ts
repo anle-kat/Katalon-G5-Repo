909b2779669f0c6e3040b232ced37b2730ce3863
TuanNguyen-Dev14 <tuan.nguyenhuu.106902@gmail.com>
2023-04-04 11:32:47 +0700

    remove

--- test-suites/G5 TS 3.ts
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<TestSuiteEntity>
-    <name>G5 TS 3</name>
-    <testSuiteGuid>f3f2fedc-31a9-4a50-872d-c0a0d45ecd23</testSuiteGuid>
-    <testCaseLink>
-        <testCaseId>test-cases/G5 Test Case</testCaseId>
-        <guid>1d891bc5-6a9a-427a-8212-0c3bb364ddf7</guid>
-    </testCaseLink>
-</TestSuiteEntity>
