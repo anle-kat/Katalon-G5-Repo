91a3e4fc0cb6d7d9714ac041baf0f2b1345d25c0
TuanNguyen-Dev14 <79506582+TuanNguyen-Dev14@users.noreply.github.com>
2023-04-04 11:39:53 +0700

    create g5 ts

---
+++ test-suites/G5 TS 1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>G5 TS 1</name>
+    <testSuiteGuid>03de762a-b96d-4d88-a912-465f50c7352d</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/G5 TC 1</testCaseId>
+        <guid>48f489b7-fbc6-4a7a-a41b-d695370ee420</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/G5 TC 2</testCaseId>
+        <guid>df3ad176-b5e6-4bbc-aa08-133194b34664</guid>
+    </testCaseLink>
+</TestSuiteEntity>
